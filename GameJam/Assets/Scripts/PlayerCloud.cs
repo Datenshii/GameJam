0fc6c789feb8cde41475f8b81b022a4913fe8fb4
FailedLilCatGod <gabriel.arturo@enti.cat>
2019-04-24 20:50:30 +0200

    Cloud added

---
+++ GameJam/Assets/Scripts/PlayerCloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCloud : MonoBehaviour {
+
+    private float h = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.Translate(Time.deltaTime * h, 0, 0);
+    }
+}
