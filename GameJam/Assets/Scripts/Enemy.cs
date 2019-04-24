71ecef7067abd00c3a5bc70c1dd05dd6ef4268ad
Datenshi <Datenshii@users.noreply.github.com>
2019-04-24 19:59:14 +0200

    24/04/19.1958

--- GameJam/Assets/Scripts/Enemy.cs
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -7,13 +7,22 @@ public class Enemy : MonoBehaviour {
     public float moveForce = 50f;
     public float maxSpeed = 2f;
 
-    private float h = 0f;
+    private float h;
+    private bool facingRight;
     private Rigidbody2D rb2d;
 
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        if (Random.Range(0, 2) > 0) h = 1;
-        else h = -1;
+        if (Random.Range(0, 2) > 0)
+        {
+            h = 1;
+            facingRight = true;
+        }
+        else
+        {
+            h = -1;
+            facingRight = false;
+        }
 	}
 	
 	void Update () {
@@ -23,6 +32,16 @@ public class Enemy : MonoBehaviour {
     void FixedUpdate() {
         if (h * rb2d.velocity.x < maxSpeed) rb2d.AddForce(Vector2.right * h * moveForce);
         if (Mathf.Abs(rb2d.velocity.x) > maxSpeed) rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+    }
 
+    public void Flip()
+    {
+        h *= -1;
+        facingRight = !facingRight;
+        //transform.Translate(Vector3.left * Time.deltaTime);
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
---
+++ GameJam/Assets/Scripts/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            enemy.Flip();
+        }
+    }
+}
