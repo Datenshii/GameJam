9eff1f64e77b23813e0a4243ff79a1e178d9b2d7
Datenshi <Datenshii@users.noreply.github.com>
2019-04-25 17:34:00 +0200

    Revert "rayolul"
    This reverts commit 089ebe9839e3366134f080dc89b1ff1eba43d893.

--- GameJam/Assets/Scripts/Enemy.cs
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour {
     private float flipCd;
     private Rigidbody2D rb2d;
 
-    void Start() {
+	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
 
         h = 1f;
@@ -21,25 +21,20 @@ public class Enemy : MonoBehaviour {
         {
             Flip();
         }
-    }
-
-    void Update() {
-
-    }
+	}
+	
+	void Update () {
+		
+	}
 
     void FixedUpdate() {
         flipCd -= Time.deltaTime;
 
-        if (flipCd <= 0) Flip();
+        if(flipCd <= 0) Flip();
 
         if (h * rb2d.velocity.x < maxSpeed) rb2d.AddForce(Vector2.right * h * moveForce);
         if (Mathf.Abs(rb2d.velocity.x) > maxSpeed) rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 
-    }
-
-    public void SmiteHit()
-    {
-        Destroy(gameObject);
     }
 
     public void Flip()
--- GameJam/Assets/Scripts/Powers.cs
+++ GameJam/Assets/Scripts/Powers.cs
@@ -4,35 +4,13 @@ using UnityEngine;
 
 public class Powers : MonoBehaviour {
 
-    public Transform SmitePoint;
-    public GameObject SmiteObject;
-
-    private float powerCd;
-
-
 	// Use this for initialization
 	void Start () {
-        powerCd = 1f;
+		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
-        powerCd -= Time.deltaTime;
-        if (Input.GetButtonDown("Fire1") && powerCd <= 0)
-        {
-            Smite();
-            powerCd = 1f;
-        }
-    }
-
-    void FixedUpdate()
-    {
-        
-    }
-
-    void Smite ()
-    {
-        Instantiate(SmiteObject, SmitePoint.position, SmitePoint.rotation);
-    }
+	void Update () {
+		
+	}
 }
