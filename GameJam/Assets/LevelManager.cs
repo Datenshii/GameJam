ba6bbc79a6b40d266e25b2f53b3b0b8da942e87b
Datenshi <Datenshii@users.noreply.github.com>
2019-04-25 17:01:51 +0200

    151456

---
+++ GameJam/Assets/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject enemy;
+    public int maxEnemies = 15;
+
+    private int numEnemies;
+    private Vector3[] spawnPositions = new Vector3[6];
+
+	void Start () {
+        spawnPositions[0] = new Vector3(10f, -4f, 0f);
+        spawnPositions[1] = new Vector3(-10f, -4f, 0f);
+        spawnPositions[2] = new Vector3(-0.75f, -3.5f, 0f);
+        spawnPositions[3] = new Vector3(-7.65f, -3.5f, 0f);
+        spawnPositions[4] = new Vector3(-5.5f, -3.5f, 0f);
+        spawnPositions[5] = new Vector3(3.5f, -3.5f, 0f);
+    }
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        numEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (numEnemies < maxEnemies){
+            Instantiate(enemy, spawnPositions[Random.Range(0,4)], Quaternion.identity);
+        }
+    }
+}
