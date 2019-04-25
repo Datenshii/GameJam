e5c93353f6825a081bfdaa072b4794b7f018b258
Datenshi <Datenshii@users.noreply.github.com>
2019-04-25 17:20:00 +0200

    21514356

--- GameJam/Assets/LevelManager.cs
+++ GameJam/Assets/LevelManager.cs
@@ -9,14 +9,23 @@ public class LevelManager : MonoBehaviour {
 
     private int numEnemies;
     private Vector3[] spawnPositions = new Vector3[6];
+    private float spawnCd;
 
-	void Start () {
+    void Start () {
+
         spawnPositions[0] = new Vector3(10f, -4f, 0f);
         spawnPositions[1] = new Vector3(-10f, -4f, 0f);
         spawnPositions[2] = new Vector3(-0.75f, -3.5f, 0f);
         spawnPositions[3] = new Vector3(-7.65f, -3.5f, 0f);
         spawnPositions[4] = new Vector3(-5.5f, -3.5f, 0f);
         spawnPositions[5] = new Vector3(3.5f, -3.5f, 0f);
+
+        spawnCd = 2f;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(enemy, spawnPositions[Random.Range(0, 4)], Quaternion.identity);
+        }
     }
 	
 	void Update () {
@@ -24,9 +33,15 @@ public class LevelManager : MonoBehaviour {
 	}
 
     void FixedUpdate() {
-        numEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        if (numEnemies < maxEnemies){
-            Instantiate(enemy, spawnPositions[Random.Range(0,4)], Quaternion.identity);
+        spawnCd -= Time.deltaTime;
+        if (spawnCd <= 0f)
+        {
+            numEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            if (numEnemies < maxEnemies)
+            {
+                Instantiate(enemy, spawnPositions[Random.Range(0, 4)], Quaternion.identity);
+            }
+            spawnCd = 2f;
         }
     }
 }
