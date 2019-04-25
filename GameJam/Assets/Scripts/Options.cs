a4744a33451ca03676adbcd57af1a7b4ad779528
Datenshii <47791059+Datenshii@users.noreply.github.com>
2019-04-25 23:44:14 +0200

    Menu Acabado
    Menu acabado y botones funcionales

---
+++ GameJam/Assets/Scripts/Options.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public void LoadSceneOnClick(int sceneNo)
+    {
+        SceneManager.LoadScene(sceneNo);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
