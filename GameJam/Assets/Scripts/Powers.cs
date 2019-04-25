089ebe9839e3366134f080dc89b1ff1eba43d893
FailedLilCatGod <47790996+FailedLilCatGod@users.noreply.github.com>
2019-04-25 17:20:15 +0200

    rayolul

--- GameJam/Assets/Scripts/Powers.cs
+++ GameJam/Assets/Scripts/Powers.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Powers : MonoBehaviour {
 
+    public Transform SmitePoint;
+    public GameObject SmiteObject;
+
+    private float powerCd;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        powerCd = 1f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        powerCd -= Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && powerCd <= 0)
+        {
+            Smite();
+            powerCd = 1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Smite ()
+    {
+        Instantiate(SmiteObject, SmitePoint.position, SmitePoint.rotation);
+    }
 }
