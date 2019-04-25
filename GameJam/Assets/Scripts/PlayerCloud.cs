fdeb51aac9b0c117b6383c184a1409907d67e0e1
Datenshi <Datenshii@users.noreply.github.com>
2019-04-25 15:56:22 +0200

    25042019

--- GameJam/Assets/Scripts/PlayerCloud.cs
+++ GameJam/Assets/Scripts/PlayerCloud.cs
@@ -6,16 +6,27 @@ public class PlayerCloud : MonoBehaviour {
 
     private float h = 5f;
 
-	// Use this for initialization
-	void Start ()
+    void Start()
     {
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+    }
 
+    void Update()
+    {
+        if (transform.position.x >= 8 || transform.position.x <= -8)
+        {
+            Flip();
+        }
         transform.Translate(Time.deltaTime * h, 0, 0);
     }
+
+    public void Flip()
+    {
+        h *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
 }
