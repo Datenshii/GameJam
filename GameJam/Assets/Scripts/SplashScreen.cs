767b64b93d13c5fcf18e3f8ad559287ce763fc32
Datenshi <Datenshii@users.noreply.github.com>
2019-04-26 19:46:16 +0200

    231231

--- GameJam/Assets/Scripts/SplashScreen.cs
+++ GameJam/Assets/Scripts/SplashScreen.cs
@@ -3,41 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour {
+public class SplashScreen : MonoBehaviour
+{
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
     public AudioClip Trueno;
 
     private AudioSource PistaAudio;
     private bool clicked = false;
     private float cd;
-=======
-    private AudioSource PistaAudio;
-    public AudioClip Trueno;
->>>>>>> ad7f88478080ecce122cc89922b47def790743a9
-=======
-
-    private AudioSource PistaAudio;
-    public AudioClip Trueno;
-
->>>>>>> 995ba5e98271383ac7fcbe6a05a0bf65cf1665c2
 
     private void Start()
     {
-        PistaAudio = GetComponent<AudioSource> ();
+        PistaAudio = GetComponent<AudioSource>();
         cd = 3f;
     }
 
-    void Update () {
+    void Update()
+    {
         if (clicked) cd -= Time.deltaTime;
 
-		if(Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1"))
+        {
             PistaAudio.clip = Trueno;
-            PistaAudio.Play ();
+            PistaAudio.Play();
             clicked = true;
         }
-        if(cd <= 0) SceneManager.LoadSceneAsync("MainMenu");
+        if (cd <= 0) SceneManager.LoadSceneAsync("MainMenu");
     }
 }
