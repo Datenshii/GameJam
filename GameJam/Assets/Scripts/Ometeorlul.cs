995ba5e98271383ac7fcbe6a05a0bf65cf1665c2
BlackRiber <pol.ribera@enti.cat>
2019-04-26 19:19:25 +0200

    explooooosion

--- GameJam/Assets/Scripts/Ometeorlul.cs
+++ GameJam/Assets/Scripts/Ometeorlul.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Ometeorlul : MonoBehaviour
 {
     public Omegasplosionlul omegasplosionlul;
+    private AudioSource PistaAudio;
+    public AudioClip Explosion;
+
+    void Start()
+    {
+        PistaAudio = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +19,8 @@ public class Ometeorlul : MonoBehaviour
 
         if (enemy != null || collision.CompareTag("Ground"))
         {
+            PistaAudio.clip = Explosion;
+            PistaAudio.Play();
             Instantiate(omegasplosionlul, transform.position, transform.rotation);
             Destroy(gameObject);
         }
--- GameJam/Assets/Scripts/SplashScreen.cs
+++ GameJam/Assets/Scripts/SplashScreen.cs
@@ -5,12 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour {
 
-<<<<<<< HEAD
+
     private AudioSource PistaAudio;
     public AudioClip Trueno;
-=======
 
->>>>>>> b95bdef44e7524d7eeb748b2777934027783f3f1
 
     private void Start()
     {
